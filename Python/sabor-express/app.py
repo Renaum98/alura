472105e699cd373ba2d46fbdceb3a119dab0242a
Renaum98 <167568624+Renaum98@users.noreply.github.com>
2025-03-18 12:28:13 -0300

    q

--- Python/sabor-express/app.py
+++ Python/sabor-express/app.py
@@ -1,4 +1,7 @@
-print('''
+import os
+
+def exibir_nome_programa():
+      print('''
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗
 ╚█████╗░███████║██████╦╝██║░░██║██████╔╝
@@ -11,13 +14,36 @@ print('''
 █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
 ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
 ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
-╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
-      ''')
+╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░''')
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Ativar Sair\n')
+def exibir_opcoes():
+      print('1. Cadastrar restaurante')
+      print('2. Listar restaurante')
+      print('3. Ativar restaurante')
+      print('4. Ativar Sair\n')
 
-opc = int(input('Escolha uma opção:'))
-print(f'Você escolheu a opcção: {opc}')
+def escolher_opcao():
+      opc = int(input('Escolha uma opção:'))
+      print(f'\nVocê escolheu a opcção: {opc}')
+
+      def finalizar_app():
+            os.system('cls')#Limpar a tela do sistema 
+            print('Finalizando o app\n') 
+
+      if opc == 1:
+            print('Cadastrar restaurante')
+      elif opc == 2:
+            print('Listar restaurante')
+      elif opc == 3:
+            print('Ativar restaurante')
+      else:
+            finalizar_app()
+    
+def main():
+    exibir_nome_programa()
+    exibir_opcoes()
+    escolher_opcao()  
+
+
+if __name__ == '__main__':
+    main()
