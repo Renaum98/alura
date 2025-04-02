9a0bfc51217668b5c07526e8f5d7a57caef67c42
Renaum98 <167568624+Renaum98@users.noreply.github.com>
2025-04-02 16:43:39 -0300

    q

--- Javascript/Projeto 5/scripts.js
+++ Javascript/Projeto 5/scripts.js
@@ -16,3 +16,75 @@ function lerConteudoDoArquivo(arquivo) {
         leitor.readAsDataURL(arquivo)
     })
 }
+
+const imagemPrincipal = document.querySelector('.main-imagem')
+const nomeDaImagem = document.querySelector('.container-imagem-name p')
+
+inputUpload.addEventListener("change", async (evento) => {
+    const arquivo = evento.target.files[0];
+
+    if (arquivo) {
+        try{
+            const conteudoDoArquivo = await lerConteudoDoArquivo(arquivo);
+            imagemPrincipal.src = conteudoDoArquivo.url;
+            nomeDaImagem.textContent = conteudoDoArquivo.nome
+        }catch(erro){
+            console.error('Erro na leitura do arquivo');
+        }
+    }
+})
+
+const inputTags = document.getElementById('input-tags');
+const listaTags = document.getElementById('lista-tags');
+
+listaTags.addEventListener("click", (evento) => {
+    if (evento.target.classList.contains("remove-tag")){
+        const tagQueQueremosRemover = evento.target.parentElement;
+        listaTags.removeChild(tagQueQueremosRemover);
+    }
+})
+
+const tagsDisponiveis = ['Front-end','Programação','Data Science', 'HTML', 'CSS', 'JavaScript', 'Python', 'Full-stack', 'Software Enginner'];
+
+async function verificaTagsDisaponiveis(tagTexto){
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(tagsDisponiveis.includes(tagTexto))
+        }, 1000);
+    })
+}
+
+inputTags.addEventListener("keypress", async (evento) => {
+    if (evento.key === "Enter") {
+        evento.preventDefault();
+        const tagTexto = inputTags.value.trim();
+        if (tagTexto !== "") {
+            try{
+                const tagExiste = await verificaTagsDisaponiveis(tagTexto);
+                if (tagExiste) {
+                    const tagNova = document.createElement('li');
+                    tagNova.innerHTML = `<p>${tagTexto}</p> <img src="./img/close-black.svg" class="remove-tag">`
+                    listaTags.appendChild(tagNova);
+                    inputTags.value = "";  
+                }else{
+                    alert('A tag não foi encontrada.');
+                }
+                } catch (error) {
+                    console.error('Erro ao verificar a existencia da tag.');
+                    alert('Erro ao verificar a existencia da tag. Verifique o console.')
+            }
+        }
+    }
+})
+
+const botaoPublicar = document.querySelector('.botao-publicar');
+botaoPublicar.addEventListener("click",async (evento) => {
+    evento.preventDefault();
+
+    const nomeDoProjeto = document.getElementById("nome").value;
+    const descricaoProjeto = document.getElementById("descricao").value;
+    const tagsProjeto = Array.from(listaTags.querySelectorAll("p")).map((tag) => tag.textContent);
+    console.log(nomeDoProjeto);
+    console.log(descricaoProjeto);
+    console.log(tagsProjeto);
+})
