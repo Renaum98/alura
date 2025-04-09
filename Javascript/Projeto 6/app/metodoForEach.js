c34b6451a9869880eb2bca026340da3527cf8500
Renaum98 <167568624+Renaum98@users.noreply.github.com>
2025-04-09 11:06:18 -0300

    Update metodoForEach.js

--- Javascript/Projeto 6/app/metodoForEach.js
+++ Javascript/Projeto 6/app/metodoForEach.js
@@ -3,17 +3,26 @@ const elementoParaInserirLivros = document.getElementById('livros');
 function exibirOsLivrosNaTela(listaDeLivros) {
     elementoParaInserirLivros.innerHTML = ''
     listaDeLivros.forEach(livro => {
+      let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel'
         elementoParaInserirLivros.innerHTML += `
-        <div class="livro">
-      <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-      <h2 class="livro__titulo">
+      <div class="livro">
+        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+        <h2 class="livro__titulo">
         ${livro.titulo}
-      </h2>
-      <p class="livro__descricao">${livro.autor}</p>
-      <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
-      <div class="tags">
+        </h2>
+        <p class="livro__descricao">${livro.autor}</p>
+        <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
+        <div class="tags">
         <span class="tag">${livro.categoria}</span>
       </div>
     </div>`        
     });
 }
+
+//function verificarDisponibilidadeDoLivro(livro){
+  //if (livro.quantidade > 0){
+    //return 'livro__imagens'
+
+//  }else {
+//    return 'livros__imagens indisponivel'}
+//  }
