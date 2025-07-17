831b2ba28aa04357958cd856421e722b5274035c
Renaum98 <167568624+Renaum98@users.noreply.github.com>
2025-07-17 15:53:48 -0300

    qwe

--- Javascript/Projeto 9/js/main.js
+++ Javascript/Projeto 9/js/main.js
@@ -1,5 +1,32 @@
 import ui from './ui.js'
+import api from './api.js'
 
 document.addEventListener("DOMContentLoaded", () => {
     ui.rederizarPensamentos()
+
+    const formularioPensamento = document.getElementById("pensamento-form")
+    formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
 })
+
+async function manipularSubmissaoFormulario(event) {
+    event.preventDefault()
+    const id = document.getElementById('pensamento-id').value
+    const conteudo = document.getElementById('pensamento-conteudo').value
+    const autoria = document.getElementById('pensamento-autoria').value
+
+    try {
+        if(id){
+            await api.editarPensamentos({id, conteudo, autoria})
+        }else {
+            await api.salvarPensamentos({conteudo,autoria})
+        }
+        ui.rederizarPensamentos()
+    } catch {
+        alert('Erro ao salvar pensamento')
+    }
+}
+
+const botaoCancelar = document.getElementById('botao-cancelar')
+botaoCancelar.onclick = () => {
+    document.getElementById("pensamento-form").reset()
+}
