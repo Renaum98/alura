92484d3eda3fc3b98019204a43780b8357d4d5d3
Renaum98 <167568624+Renaum98@users.noreply.github.com>
2025-07-22 14:59:35 -0300

    qwe

--- Javascript/Projeto 10/js/api.js
+++ Javascript/Projeto 10/js/api.js
@@ -12,17 +12,6 @@ const api = {
     }
   },
 
-  async salvarPensamento(pensamento) {
-    try {
-      const response = await axios.post(`${URL_BASE}/pensamentos`, pensamento)
-      return await response.data
-    }
-    catch {
-      alert('Erro ao salvar pensamento')
-      throw error
-    }
-  },
-
   async buscarPensamentoPorId(id) {
     try {
       const response = await axios.get(`${URL_BASE}/pensamentos/${id}`)
@@ -30,6 +19,17 @@ const api = {
     }
     catch {
       alert('Erro ao buscar pensamento')
+      throw error
+    }
+  },
+
+  async salvarPensamento(pensamento) {
+    try {
+      const response = await axios.post(`${URL_BASE}/pensamentos`, pensamento)
+      return await response.data
+    }
+    catch {
+      alert('Erro ao salvar pensamento')
       throw error
     }
   },
@@ -53,7 +53,25 @@ const api = {
       alert('Erro ao excluir um pensamento')
       throw error
     }
-  }
+  },
+  async buscarPensamentoPorTermo(termo){
+    try {
+      const pensamentos = await this.buscarPensamento()
+      const termoEmMinusculas = termo.toLowerCase()
+  
+      const pensamentosFiltrados = pensamentos.filter(pensamento => {
+        return (pensamento.conteudo.toLowerCase().includes(termoEmMinusculas)) || pensamento.autoria.toLowerCase().includes(termoEmMinusculas)
+      })
+      return pensamentosFiltrados
+      
+    } 
+    catch (error) {
+      alert('Erro ao filtrar pensamentos')
+      throw error
+    }
+  }   
 }
+
+
 
 export default api
