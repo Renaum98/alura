4d1351a71afb7c0edd7435b59b484ccbe23a7f16
Renaum98 <167568624+Renaum98@users.noreply.github.com>
2025-12-16 17:49:53 -0300

    upgrade

--- React/Projeto1/tecboard/src/App.jsx
+++ React/Projeto1/tecboard/src/App.jsx
@@ -1,18 +1,52 @@
 import "./App.css";
+import { Banner } from "./componentes/Banner";
 import { FormularioDeEvento } from "./componentes/FormularioDeEvento";
+import { Tema } from "./componentes/Tema";
 
 //NO REACT COMPONENTES SÃO FUNÇÕES
 
 function App() {
+  const temas = [
+    {
+      id: 1,
+      nome: "front-end",
+    },
+    {
+      id: 2,
+      nome: "back-end",
+    },
+    {
+      id: 3,
+      nome: "dev-ops",
+    },
+    {
+      id: 4,
+      nome: "Inteligência artificial",
+    },
+    {
+      id: 5,
+      nome: "data science",
+    },
+    {
+      id: 6,
+      nome: "cloud",
+    },
+  ];
+
   return (
     <main>
       <header>
         <img src="/logo.png" alt="" />
       </header>
-      <section>
-        <img src="/banner.png" alt="" />
-      </section>
+      <Banner />
       <FormularioDeEvento />
+      {temas.map(function (item) { /*o map é quase igual o forEach com a diferença que retorna um valor*/
+        return (
+          <section key={item.id}>
+            <Tema tema={item} />
+          </section>
+        );
+      })}
     </main>
   );
 }
