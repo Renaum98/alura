01f861672b487a3d5bef262134f8d37ebb3057a4
Renaum98 <167568624+Renaum98@users.noreply.github.com>
2026-01-09 11:54:46 -0300

    Update atividade2.js

--- Javascript/javascript-aprendendo-programar/atividade2.js
+++ Javascript/javascript-aprendendo-programar/atividade2.js
@@ -10,21 +10,102 @@ if (idade >= 18) {
 //2. Saudação de acordo com a hora
 const horaAtual = new Date().getHours();
 
-if (horaAtual.getHours >= 6 && horaAtual < 12) {
+if (horaAtual >= 6 && horaAtual < 12) {
   console.log("Bom dia");
-} else if (horaAtual.getHours >= 12 && horaAtual < 18) {
+} else if (horaAtual >= 12 && horaAtual < 18) {
   console.log("Boa Tarde");
 } else {
   console.log("Boa Noite");
 }
 
 //3. Verificação de número positivo ou negativo
-const num = 10
+const num = -5
+
+if (num > 0) {
+  console.log(`${num} é positivo`)
+} else if (num < 0) {
+  console.log(`${num} é Negativo`)
+} else {
+  console.log(`${num} é 0`)
+}
 
 //4. Conversão de nota em conceito
+const nota = 5
+if (nota >= 9 && nota <= 10) {
+  console.log('Sua nota foi A');
+} else if (nota >= 8 && nota < 9) {
+  console.log('Sua nota foi B');
+} else if (nota >= 6 && nota <= 7.9) {
+  console.log('Sua nota foi C');
+} else if (nota >= 4 && nota <= 5.9) {
+  console.log('Sua nota foi D')
+} else if (nota >= 0 && nota <= 3.9) {
+  console.log('Sua nota foi E')
+}
 //5. Número par ou ímpar com ternário
+
+let numero2 = 7; 
+
+let resultado = (numero2 % 2 === 0) ? "Par" : "Ímpar"; 
+
+console.log(`O número é: ${resultado}`); 
+
 //6. Menu com switch-case
+const opcao = 3
+switch (opcao) {
+  case 1:
+    console.log('Cadastrar')
+    break;
+  case 2:
+    console.log('Listar')
+    break
+  case 3:
+    console.log('Sair')
+    break
+  default:
+    break;
+}
+
 //7. Validação de campo obrigatório
+const email = ""
+if (!email) {
+  console.log('Cadastre um email')
+} else {
+  console.log('E-mail válido')
+}
+
 //8. Validação de senha segura
+const senha = ""
+const senhaValida = false
+
+if (senhaValida) {
+  console.log('Senha Valida')
+} else {
+  console.log('Senha muito curta')
+}
+
+
 //9. Compra com saldo
+const saldoDisponivel = 250.10
+const valorCompra = 135.95
+if (saldoDisponivel < valorCompra) {
+  console.log(`
+    Saldo indisponivel
+    O valor da compra é de ${valorCompra} e seu saldo é ${saldoDisponivel}`)
+} else {
+  console.log(`
+    Compra aprovada no valor:
+    R$${valorCompra}
+    Seu novo saldo é de R$${saldoDisponivel - valorCompra}`)
+}
 //10. Validação de formulário completo
+const nome = ''
+const email2 = ''
+const anos = ''
+const formularioValido = true
+
+if (formularioValido) {
+  console.log("Formulário enviado com sucesso");
+} else {
+  console.log("Por favor, preencha todos os campos corretamente");
+}
